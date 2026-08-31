b2ea1222eb37895ebd9506f96f8583aa330d5258
Ami <sitinorsrirahmi@gmail.com>
2026-08-31 14:29:39 +0800

    Tampilkan gambar di show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -7,29 +7,35 @@
 
     <div class="py-12">
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 space-y-3">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
 
-                <div>
-                    <span class="font-medium text-gray-700">Nama:</span>
-                    <p>{{ $product->nama }}</p>
+                @if ($product->getFirstMediaUrl('images'))
+                    <img src="{{ $product->getFirstMediaUrl('images') }}" class="w-48 h-48 object-cover rounded mb-6">
+                @endif
+
+                <div class="grid grid-cols-2 gap-x-6 gap-y-4">
+                    <div>
+                        <span class="font-bold text-black-700">Nama</span>
+                        <p>{{ $product->nama }}</p>
+                    </div>
+
+                    <div>
+                        <span class="font-bold text-black-700">Deskripsi</span>
+                        <p>{{ $product->deskripsi }}</p>
+                    </div>
+
+                    <div>
+                        <span class="font-bold text-black-700">Harga</span>
+                        <p>Rp{{ number_format($product->harga) }}</p>
+                    </div>
+
+                    <div>
+                        <span class="font-bold text-black-700">Stok</span>
+                        <p>{{ $product->stok }}</p>
+                    </div>
                 </div>
 
-                <div>
-                    <span class="font-medium text-gray-700">Deskripsi:</span>
-                    <p>{{ $product->deskripsi }}</p>
-                </div>
-
-                <div>
-                    <span class="font-medium text-gray-700">Harga:</span>
-                    <p>Rp{{ number_format($product->harga) }}</p>
-                </div>
-
-                <div>
-                    <span class="font-medium text-gray-700">Stok:</span>
-                    <p>{{ $product->stok }}</p>
-                </div>
-
-                <a href="{{ route('products.index') }}" class="inline-block mt-4 text-blue-600">Kembali</a>
+                <a href="{{ route('products.index') }}" class="inline-block mt-6 text-blue-600">Kembali</a>
 
             </div>
         </div>
