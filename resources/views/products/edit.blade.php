7017b400d87e9d7bca3eec7933ee98fee059ff3b
Ami <sitinorsrirahmi@gmail.com>
2026-09-05 14:08:25 +0800

    Tambah input gambar pada form edit produk

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
 
-                <form action="{{ route('products.update', $product->id) }}" method="POST" enctype="multipart/form-data class="space-y-4">
+                <form action="{{ route('products.update', $product->id) }}" method="POST" enctype="multipart/form-data" class="space-y-4">
                     @csrf
                     @method('PUT')
 
